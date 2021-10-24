2960aa24f1c1f4633075eec62a81587e39411b81
Manu84-bit <megaskatos@gmail.com>
2021-10-23 22:00:19 -0500

    Using lodash to manipulate arrays

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 //store info about the project, like the modules it uses (to locally install new modules, use npm i package_name).
 //In this case the lodash package was installed
 
+//npm i nodemon -D installs nodemon as a Devdependency, just for development.
+
 const _ = require("lodash");
 
 const items = [1, [2, [3, [4]]]];
